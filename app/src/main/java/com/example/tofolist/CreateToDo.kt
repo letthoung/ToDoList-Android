e0b77d039334a8e6a5e96fa2bbac43336ba28dfb
letthoung <thanhtung3496@gmail.com>
2020-07-17 16:42:20 -0400

    Added the function to mark a todo item as completed

--- app/src/main/java/com/example/tofolist/CreateToDo.kt
+++ app/src/main/java/com/example/tofolist/CreateToDo.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import kotlinx.android.synthetic.main.activity_create_to_do.*
 
 class CreateToDo : AppCompatActivity() {
@@ -11,19 +12,35 @@ class CreateToDo : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_to_do)
 
-        button.setOnClickListener {
-            var todoDesc = "";
-            if (checkBox.isChecked) {
-                todoDesc = "!" + editText.text.toString();
-            } else {
-                todoDesc = editText.text.toString();
+        if (intent.hasExtra("todoDesc")){
+            val todo = intent.extras?.getString("todoDesc");
+            editText.setText(todo);
+            editText.isEnabled = false;
+            checkBox.visibility = View.INVISIBLE;
+            button.setText("COMPLETE");
+            button.setOnClickListener{
+                var prefs = getSharedPreferences("com.example.todolist.sharedprefs",Context.MODE_PRIVATE);
+                var todos = prefs.getStringSet("todos", setOf())?.toMutableSet();
+                todos?.remove(todo);
+                prefs.edit().putStringSet("todos", todos).apply();
+                finish();
             }
-            var prefs = getSharedPreferences("com.example.todolist.sharedprefs", Context.MODE_PRIVATE);
-            var todos = prefs.getStringSet("todos", setOf())?.toMutableSet();
-            todos?.add(todoDesc);
-            Log.d("todoList", todos.toString());
-            prefs.edit().putStringSet("todos",todos).apply();
-            finish();
+        } else {
+            button.setOnClickListener {
+                var todoDesc = "";
+                if (checkBox.isChecked) {
+                    todoDesc = "!" + editText.text.toString();
+                } else {
+                    todoDesc = editText.text.toString();
+                }
+                var prefs =
+                    getSharedPreferences("com.example.todolist.sharedprefs", Context.MODE_PRIVATE);
+                var todos = prefs.getStringSet("todos", setOf())?.toMutableSet();
+                todos?.add(todoDesc);
+                Log.d("todoList", todos.toString());
+                prefs.edit().putStringSet("todos", todos).apply();
+                finish();
+            }
         }
     }
 }
--- app/src/main/java/com/example/tofolist/ToDoAdapter.kt
+++ app/src/main/java/com/example/tofolist/ToDoAdapter.kt
@@ -1,19 +1,31 @@
 package com.example.tofolist
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.to_do_row.view.*
 
 class ToDoAdapter(val todos: List<String>): RecyclerView.Adapter<ToDoAdapter.ToDoHolder>() {
-    class ToDoHolder(v: View): RecyclerView.ViewHolder(v) {
+    class ToDoHolder(v: View): RecyclerView.ViewHolder(v), View.OnClickListener {
         var view: View = v;
         var todoDesc: String = "";
 
         fun bindToDo(todoDesc: String) {
             this.todoDesc = todoDesc;
             view.textView.text = todoDesc;
+        }
+
+        init {
+            v.setOnClickListener(this)
+        }
+
+        override fun onClick(p0: View?) {
+            val intent = Intent(view.context,CreateToDo::class.java);
+            intent.putExtra("todoDesc", todoDesc);
+            startActivity(view.context, intent, null);
         }
     }
 
